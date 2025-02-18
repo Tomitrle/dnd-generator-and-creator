bea160c4cbc93d5673910b151da2f5d6e48027b3
Brennen Muller <mullerbrennen@gmail.com>
2025-02-18 17:37:32 -0500

    Create homepage with PHP 'include'
    Template documents contain 'TODO' comments as needed. Further work to be done in branches.

---
+++ SOURCES.md
@@ -0,0 +1,11 @@
+# Sources Cited
+This file should be reformatted at a later date.
+
+For now, list any specific resources alongside the applicable code. All  _generic_ or _multi-purpose_ sources should also be listed below:
+
+## Documentation
+- Bootstrap Documentation: https://getbootstrap.com/docs/5.3/
+- Bootstrap Examples: https://getbootstrap.com/docs/5.3/examples/
+
+## Generative AI
+- ChatGPT: https://chatgpt.com/
---
+++ web/www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- https://www.w3schools.com/PHP/php_includes.asp -->
+<?php include '/opt/src/templates/base.html';?>
+
+<body>
+  <?php include '/opt/src/templates/navbar.html';?>
+  <header class="container p-4 text-center">
+    <h1>
+      D&D Encounter Generator and Monster Creator
+    </h1>
+  </header>
+
+  <?php include '/opt/src/templates/footer.html';?>
+  <?php include '/opt/src/templates/base-javascript.html';?>
+</body>
+</html>
