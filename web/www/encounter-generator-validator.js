4c0f2611a178f3f50fc53366a932979422b0ceb4
Tommy Le <113615061+Tomitrle@users.noreply.github.com>
2025-02-22 18:03:03 -0500

    Set up encounter-generator.html

---
+++ web/www/encounter-generator-validator.js
@@ -0,0 +1,17 @@
+// Source: https://getbootstrap.com/docs/5.3/forms/validation/
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
