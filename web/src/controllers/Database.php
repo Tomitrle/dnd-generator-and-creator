dfa04987455f87de6d0ce238c93827c320306d29
Brennen Muller <mullerbrennen@gmail.com>
2025-03-25 18:25:00 -0400

    Database tables setup

--- web/src/controllers/Database.php
+++ web/src/controllers/Database.php
@@ -12,32 +12,70 @@ class Database
         $port = Config::$db["port"];
 
         $this->connection = pg_connect("host=$host port=$port dbname=$database user=$user password=$password") or die("Could not connect to the database.");
-        $this->createTables();
     }
 
     public function dropTables() : void
     {
-        // MARK: TODO
-        // pg_query($this->connection, "DROP TABLE IF EXISTS ... CASCADE;");
+        pg_query($this->connection, "DROP TABLE IF EXISTS dnd_users CASCADE;");
+        pg_query($this->connection, "DROP TABLE IF EXISTS dnd_monsters CASCADE;");
+        pg_query($this->connection, "DROP TABLE IF EXISTS dnd_attributes CASCADE;");
     }
 
     public function createTables() : void
     {
-        // MARK: TODO
-//         $result = pg_query($this->connection, "CREATE TABLE IF NOT EXISTS hw6_users (
-//             id          SERIAL PRIMARY KEY,
-//             name        TEXT,
-//             email       TEXT,
-//             password    TEXT
-//             );");
-//
-//         $result = pg_query($this->connection, "CREATE TABLE IF NOT EXISTS ... (
-//             id          SERIAL PRIMARY KEY,
-//             userID      INT REFERENCES hw6_users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-//             word        TEXT,
-//             victory     BOOLEAN,
-//             score       INT
-//             );");
+        $result = pg_query($this->connection, "CREATE TABLE IF NOT EXISTS dnd_users (
+            id              SERIAL PRIMARY KEY,
+            username        TEXT,
+            password        TEXT
+            );");
+
+        $result = pg_query($this->connection, "CREATE TABLE IF NOT EXISTS dnd_monsters (
+            id              SERIAL PRIMARY KEY,
+            userID          INT REFERENCES dnd_users(id) ON DELETE CASCADE,
+            name            TEXT,
+            size            TEXT,
+            type            TEXT,
+            alignment       TEXT,
+
+            armor           TEXT,
+            shield          BOOLEAN,
+            armorClass      INT,
+            hitDice         INT,
+            health          INT,
+
+            speed           INT,
+
+            strength        INT,
+            dexterity       INT,
+            constitution    INT,
+            intelligence    INT,
+            wisdom          INT,
+            charmisma       INT,
+
+            strengthSave        BOOLEAN,
+            dexteritySave       BOOLEAN,
+            constitutionSave    BOOLEAN,
+            intelligenceSave    BOOLEAN,
+            wisdomSave          BOOLEAN,
+            charmismaSave       BOOLEAN,
+
+            blind           BOOLEAN,
+            telepathy       INT,
+
+            challenge       INT
+            );");
+
+        $result = pg_query($this->connection, "CREATE TABLE IF NOT EXISTS dnd_attributes (
+            id              SERIAL PRIMARY KEY,
+            monsterID       INT REFERENCES dnd_monsters(id) ON DELETE CASCADE,
+            type            TEXT,
+            name            TEXT,
+
+            range           INT,
+
+            description     TEXT,
+            benefit         INT
+            );");
     }
 
     public function query($query, ...$params) : bool | array
--- web/www/database-setup.php
+++ web/www/database-setup.php
@@ -5,15 +5,16 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 spl_autoload_register(function ($classname) {
-  require "/opt/src/hw6/controllers/$classname.php";
+  require "/opt/src/controllers/$classname.php";
 });
 
 $database = new Database();
 $database->dropTables();
 $database->createTables();
 
-session_destroy();
 session_start();
+session_destroy();
 
-require "index.php";
+header("Location: index.php");
+exit();
 ?>
